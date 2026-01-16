c12cfdf90519d1067f879eb7d33196ab83e7d6cb
ToanPham <pnttoan1474@gmail.com>
2026-01-16 16:41:52 +0700

    Compatibility fix for older iOS/JSContext (v1.0.5)

--- 0.8/TruyenQQ/source.js
+++ 0.8/TruyenQQ/source.js
@@ -7,184 +7,213 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TruyenQQ = exports.TruyenQQInfo = void 0;
 
-const DOMAIN = 'https://truyenqqno.com';
+var DOMAIN = 'https://truyenqqno.com';
 
 // Parser class
-class Parser {
-    convertTime(timeAgo) {
-        const now = Date.now();
-        if (timeAgo.includes('phút')) {
-            const mins = parseInt(timeAgo.match(/(\d+)/)?.[1] || '0');
-            return new Date(now - mins * 60 * 1000);
-        }
-        if (timeAgo.includes('giờ')) {
-            const hours = parseInt(timeAgo.match(/(\d+)/)?.[1] || '0');
-            return new Date(now - hours * 60 * 60 * 1000);
-        }
-        if (timeAgo.includes('ngày')) {
-            const days = parseInt(timeAgo.match(/(\d+)/)?.[1] || '0');
-            return new Date(now - days * 24 * 60 * 60 * 1000);
-        }
-        if (timeAgo.includes('/')) {
-            const parts = timeAgo.split('/');
-            if (parts.length === 3) {
-                return new Date(parseInt(parts[2]), parseInt(parts[1]) - 1, parseInt(parts[0]));
-            }
-        }
-        return new Date();
-    }
+function Parser() { }
 
-    parseMangaList($) {
-        const mangas = [];
-        $('ul.list_grid li').each((_, element) => {
-            const $elem = $(element);
-            const $link = $elem.find('a').first();
-            const url = $link.attr('href');
-            if (!url) return;
-            const match = url.match(/truyen-tranh\/(.+)-(\d+)/);
-            if (!match) return;
-            const mangaId = `${match[1]}-${match[2]}`;
-            const title = $link.attr('title') || $elem.find('.book_name a').text().trim();
-            const image = $elem.find('img').attr('data-src') || $elem.find('img').attr('src') || '';
-            const subtitle = $elem.find('.chapter_name').text().trim();
-            mangas.push(App.createPartialSourceManga({
-                mangaId, title, image, subtitle: subtitle || undefined
-            }));
-        });
-        return mangas;
+Parser.prototype.convertTime = function (timeAgo) {
+    var now = Date.now();
+    if (timeAgo.indexOf('phút') !== -1) {
+        var minsMatch = timeAgo.match(/(\d+)/);
+        var mins = minsMatch ? parseInt(minsMatch[1]) : 0;
+        return new Date(now - mins * 60 * 1000);
     }
+    if (timeAgo.indexOf('giờ') !== -1) {
+        var hoursMatch = timeAgo.match(/(\d+)/);
+        var hours = hoursMatch ? parseInt(hoursMatch[1]) : 0;
+        return new Date(now - hours * 60 * 60 * 1000);
+    }
+    if (timeAgo.indexOf('ngày') !== -1) {
+        var daysMatch = timeAgo.match(/(\d+)/);
+        var days = daysMatch ? parseInt(daysMatch[1]) : 0;
+        return new Date(now - days * 24 * 60 * 60 * 1000);
+    }
+    if (timeAgo.indexOf('/') !== -1) {
+        var parts = timeAgo.split('/');
+        if (parts.length === 3) {
+            return new Date(parseInt(parts[2]), parseInt(parts[1]) - 1, parseInt(parts[0]));
+        }
+    }
+    return new Date();
+};
 
-    parseMangaDetails($, mangaId) {
-        const tags = [];
-        $('.list01 li a').each((_, elem) => {
-            const label = $(elem).text().trim();
-            const id = $(elem).attr('href')?.split('/').pop() || label;
-            tags.push(App.createTag({ label, id }));
-        });
-        const title = $('.book_info h1').text().trim();
-        const author = $('.list_info .org').first().text().trim() || 'Đang cập nhật';
-        const image = $('.book_avatar img').attr('src') || '';
-        const desc = $('.story_introduction').text().trim();
-        const status = $('.list_info li').eq(2).text().trim();
-        return App.createSourceManga({
-            id: mangaId,
-            mangaInfo: App.createMangaInfo({
-                titles: [title], author, artist: author, image, desc, status,
-                tags: [App.createTagSection({ id: '0', label: 'Thể loại', tags })]
-            })
-        });
-    }
+Parser.prototype.parseMangaList = function ($) {
+    var mangas = [];
+    var self = this;
+    $('ul.list_grid li').each(function (_, element) {
+        var $elem = $(element);
+        var $link = $elem.find('a').first();
+        var url = $link.attr('href');
+        if (!url) return;
+        var match = url.match(/truyen-tranh\/(.+)-(\d+)/);
+        if (!match) return;
+        var mangaId = match[1] + '-' + match[2];
+        var title = $link.attr('title') || $elem.find('.book_name a').text().trim();
+        var image = $elem.find('img').attr('data-src') || $elem.find('img').attr('src') || '';
+        var subtitle = $elem.find('.chapter_name').text().trim();
+        mangas.push(App.createPartialSourceManga({
+            mangaId: mangaId,
+            title: title,
+            image: image,
+            subtitle: subtitle || undefined
+        }));
+    });
+    return mangas;
+};
 
-    parseChapterList($) {
-        const chapters = [];
-        $('.list_chapter .works-chapter-item').each((_, element) => {
-            const $elem = $(element);
-            const $link = $elem.find('a').first();
-            const url = $link.attr('href');
-            if (!url) return;
-            const chapterName = $link.find('.name-chap').text().trim();
-            const timeText = $elem.find('.time-chap').text().trim();
-            const chapMatch = url.match(/chap-(\d+(?:\.\d+)?)/);
-            const chapNum = chapMatch ? parseFloat(chapMatch[1]) : 0;
-            const time = this.convertTime(timeText);
-            chapters.push(App.createChapter({
-                id: url, name: chapterName, chapNum, langCode: 'vi', time
-            }));
-        });
-        return chapters;
-    }
+Parser.prototype.parseMangaDetails = function ($, mangaId) {
+    var tags = [];
+    $('.list01 li a').each(function (_, elem) {
+        var label = $(elem).text().trim();
+        var href = $(elem).attr('href');
+        var id = href ? href.split('/').pop() : label;
+        tags.push(App.createTag({ label: label, id: id }));
+    });
+    var title = $('.book_info h1').text().trim();
+    var author = $('.list_info .org').first().text().trim() || 'Đang cập nhật';
+    var image = $('.book_avatar img').attr('src') || '';
+    var desc = $('.story_introduction').text().trim();
+    var status = $('.list_info li').eq(2).text().trim();
+    return App.createSourceManga({
+        id: mangaId,
+        mangaInfo: App.createMangaInfo({
+            titles: [title],
+            author: author,
+            artist: author,
+            image: image,
+            desc: desc,
+            status: status,
+            tags: [App.createTagSection({ id: '0', label: 'Thể loại', tags: tags })]
+        })
+    });
+};
 
-    parseChapterDetails($) {
-        const pages = [];
-        $('#chapter_content .page-chapter img, .chapter_content .page-chapter img').each((_, element) => {
-            const imageUrl = $(element).attr('data-original') || $(element).attr('data-cdn') || $(element).attr('src') || '';
-            if (imageUrl && imageUrl.startsWith('http')) {
-                pages.push(imageUrl);
-            }
-        });
-        return pages;
-    }
-}
+Parser.prototype.parseChapterList = function ($) {
+    var chapters = [];
+    var self = this;
+    $('.list_chapter .works-chapter-item').each(function (_, element) {
+        var $elem = $(element);
+        var $link = $elem.find('a').first();
+        var url = $link.attr('href');
+        if (!url) return;
+        var chapterName = $link.find('.name-chap').text().trim();
+        var timeText = $elem.find('.time-chap').text().trim();
+        var chapMatch = url.match(/chap-(\d+(?:\.\d+)?)/);
+        var chapNum = chapMatch ? parseFloat(chapMatch[1]) : 0;
+        var time = self.convertTime(timeText);
+        chapters.push(App.createChapter({
+            id: url,
+            name: chapterName,
+            chapNum: chapNum,
+            langCode: 'vi',
+            time: time
+        }));
+    });
+    return chapters;
+};
+
+Parser.prototype.parseChapterDetails = function ($) {
+    var pages = [];
+    $('#chapter_content .page-chapter img, .chapter_content .page-chapter img').each(function (_, element) {
+        var imageUrl = $(element).attr('data-original') || $(element).attr('data-cdn') || $(element).attr('src') || '';
+        if (imageUrl && imageUrl.indexOf('http') === 0) {
+            pages.push(imageUrl);
+        }
+    });
+    return pages;
+};
 
 // SourceInfo
 exports.TruyenQQInfo = {
-    version: '1.0.0',
+    version: '1.0.5',
     name: 'TruyenQQ',
     icon: 'icon.png',
     author: 'Paperback Community',
     authorWebsite: 'https://github.com/paperback-community',
-    description: 'Extension cho trang TruyenQQ',
+    description: 'Extension cho trang TruyenQQ (Compatibility Fix)',
     contentRating: 1, // MATURE
     websiteBaseURL: DOMAIN,
     sourceTags: [
         { text: 'Vietnamese', type: 0 },
         { text: 'Manhwa', type: 1 }
     ],
-    intents: 17  // MANGA_CHAPTERS (1) + CLOUDFLARE_BYPASS_REQUIRED (16)
+    intents: 17 // MANGA_CHAPTERS (1) + CLOUDFLARE_BYPASS_REQUIRED (16)
 };
 
 // Main class
-class TruyenQQ {
-    constructor(cheerio) {
-        this.cheerio = cheerio;
-        this.parser = new Parser();
-        this.requestManager = App.createRequestManager({
-            requestsPerSecond: 4,
-            requestTimeout: 15000,
-            interceptor: {
-                interceptRequest: async (request) => {
-                    request.headers = {
-                        ...(request.headers ?? {}),
-                        'referer': DOMAIN,
-                        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
-                    };
-                    return request;
-                },
-                interceptResponse: async (response) => response
+function TruyenQQ(cheerio) {
+    this.cheerio = cheerio;
+    this.parser = new Parser();
+    var self = this;
+    this.requestManager = App.createRequestManager({
+        requestsPerSecond: 4,
+        requestTimeout: 15000,
+        interceptor: {
+            interceptRequest: function (request) {
+                var headers = request.headers || {};
+                headers['referer'] = DOMAIN;
+                headers['user-agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148';
+                request.headers = headers;
+                return Promise.resolve(request);
+            },
+            interceptResponse: function (response) {
+                return Promise.resolve(response);
             }
-        });
-    }
+        }
+    });
+}
 
-    getMangaShareUrl(mangaId) {
-        return `${DOMAIN}/truyen-tranh/${mangaId}`;
-    }
+TruyenQQ.prototype.getMangaShareUrl = function (mangaId) {
+    return DOMAIN + '/truyen-tranh/' + mangaId;
+};
 
-    async DOMHTML(url) {
-        const request = App.createRequest({ url, method: 'GET' });
-        const response = await this.requestManager.schedule(request, 1);
-        return this.cheerio.load(response.data);
-    }
+TruyenQQ.prototype.DOMHTML = function (url) {
+    var self = this;
+    var request = App.createRequest({ url: url, method: 'GET' });
+    return this.requestManager.schedule(request, 1).then(function (response) {
+        return self.cheerio.load(response.data);
+    });
+};
 
-    async getMangaDetails(mangaId) {
-        const $ = await this.DOMHTML(`${DOMAIN}/truyen-tranh/${mangaId}`);
-        return this.parser.parseMangaDetails($, mangaId);
-    }
+TruyenQQ.prototype.getMangaDetails = function (mangaId) {
+    var self = this;
+    return this.DOMHTML(DOMAIN + '/truyen-tranh/' + mangaId).then(function ($) {
+        return self.parser.parseMangaDetails($, mangaId);
+    });
+};
 
-    async getChapters(mangaId) {
-        const $ = await this.DOMHTML(`${DOMAIN}/truyen-tranh/${mangaId}`);
-        return this.parser.parseChapterList($);
-    }
+TruyenQQ.prototype.getChapters = function (mangaId) {
+    var self = this;
+    return this.DOMHTML(DOMAIN + '/truyen-tranh/' + mangaId).then(function ($) {
+        return self.parser.parseChapterList($);
+    });
+};
 
-    async getChapterDetails(mangaId, chapterId) {
-        const url = chapterId.startsWith('http') ? chapterId : `${DOMAIN}${chapterId}`;
-        const $ = await this.DOMHTML(url);
-        const pages = this.parser.parseChapterDetails($);
-        return App.createChapterDetails({ id: chapterId, mangaId, pages });
-    }
+TruyenQQ.prototype.getChapterDetails = function (mangaId, chapterId) {
+    var self = this;
+    var url = chapterId.indexOf('http') === 0 ? chapterId : DOMAIN + chapterId;
+    return this.DOMHTML(url).then(function ($) {
+        var pages = self.parser.parseChapterDetails($);
+        return App.createChapterDetails({ id: chapterId, mangaId: mangaId, pages: pages });
+    });
+};
 
-    async getSearchResults(query, metadata) {
-        const page = metadata?.page ?? 1;
-        const url = query.title
-            ? `${DOMAIN}/tim-kiem.html?q=${encodeURIComponent(query.title)}`
-            : `${DOMAIN}/truyen-moi-cap-nhat/trang-${page}.html`;
-        const $ = await this.DOMHTML(url);
-        const mangas = this.parser.parseMangaList($);
-        const hasNext = $('.pagination a.next, a[title="Next"]').length > 0;
+TruyenQQ.prototype.getSearchResults = function (query, metadata) {
+    var self = this;
+    var page = (metadata && metadata.page) ? metadata.page : 1;
+    var url = (query && query.title)
+        ? DOMAIN + '/tim-kiem.html?q=' + encodeURIComponent(query.title)
+        : DOMAIN + '/truyen-moi-cap-nhat/trang-' + page + '.html';
+
+    return this.DOMHTML(url).then(function ($) {
+        var mangas = self.parser.parseMangaList($);
+        var hasNext = $('.pagination a.next, a[title="Next"]').length > 0;
         return App.createPagedResults({
             results: mangas,
             metadata: hasNext ? { page: page + 1 } : undefined
         });
-    }
-}
+    });
+};
 
 exports.TruyenQQ = TruyenQQ;
